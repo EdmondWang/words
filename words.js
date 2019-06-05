cd12a26e71e10b64492e204a20c53e3110b1203e
Edmond Wang <ywang53@ebay.com>
2019-06-05 12:50:37 +0800

    add nodemon

--- words.js
+++ words.js
@@ -1,9 +1,16 @@
 const words = `
-accord,
+accord, apple, apply, application, arm,
 build, base, brave, baker,
 cute, cow, collection, coach, coast, core, cost, code, cool, cold, com, cake, car, cast, career, convert, celebrate, catch, camera, case, cafe, coffee, cell, carbon,
-front, float, floor, forgive, for, force, forever, fragment, fire, figure, french, favourite
-honda, hire, human, hover, housr, horse, harm, have, how, however, hello,
+front, float, floor, forgive, for, force, forever, fragment, fire, figure, french, favourite,
+grade,
+honda, hire, human, hover, housr, horse, harm, have, how, however, hello, history, horizontal, happy,
+pull, push, pollute, pillar, pick, paradise,
+recent, recall, reply, real, really, reduce, red, renew,
+stage, star, scroll,
+thing, tiny, tired,
+unique,
+zoo, zoom
 `;
 
 const arr = words.split(',');
