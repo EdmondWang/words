ad9a1b6acdc9d069aaf7ecb18e6ff60b41aa6b1e
Edmond Wang <ywang53@ebay.com>
2019-06-05 12:45:46 +0800

    Initial

---
+++ words.js
@@ -0,0 +1,11 @@
+const words = `
+accord,
+build, base, brave, baker,
+cute, cow, collection, coach, coast, core, cost, code, cool, cold, com, cake, car, cast, career, convert, celebrate, catch, camera, case, cafe, coffee, cell, carbon,
+front, float, floor, forgive, for, force, forever, fragment, fire, figure, french, favourite
+honda, hire, human, hover, housr, horse, harm, have, how, however, hello,
+`;
+
+const arr = words.split(',');
+
+console.log(`Total: ${arr.length}`);
