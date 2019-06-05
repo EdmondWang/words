25e1c00e2bab7229c420f0f636b9f2703e267d4d
Edmond Wang <ywang53@ebay.com>
2019-06-05 12:56:10 +0800

    from a - z

--- words.js
+++ words.js
@@ -1,15 +1,29 @@
 const words = `
 accord, apple, apply, application, arm,
-build, base, brave, baker,
+build, base, brave, baker, big
 cute, cow, collection, coach, coast, core, cost, code, cool, cold, com, cake, car, cast, career, convert, celebrate, catch, camera, case, cafe, coffee, cell, carbon,
+dog,
+eye,
 front, float, floor, forgive, for, force, forever, fragment, fire, figure, french, favourite,
 grade,
 honda, hire, human, hover, housr, horse, harm, have, how, however, hello, history, horizontal, happy,
+icon,
+jest,
+kitchen,
+large,
+memory,
+new,
+often, omit,
 pull, push, pollute, pillar, pick, paradise,
+queue,
 recent, recall, reply, real, really, reduce, red, renew,
-stage, star, scroll,
+stage, star, scroll, style, sheet, small
 thing, tiny, tired,
 unique,
+visit,
+world,
+xerox,
+year,
 zoo, zoom
 `;
 
