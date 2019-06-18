5e1986d01b2de1730ea910202ea72bf4a0bce7ef
Edmond Wang <ywang53@ebay.com>
2019-06-18 16:37:43 +0800

    add words

--- words.js
+++ words.js
@@ -1,22 +1,22 @@
 const words = `
 accord, apple, apply, application, arm, auto, almost, animation, advertisement, alert, angry, angela, autumn, army, ask, automatic, airplane,
-aircraft, along, align, allow, abort, accept, amazing,
+aircraft, along, align, allow, abort, accept, amazing, anticlock, array,
 
 build, base, brave, baker, big, but, because, bubble, bump, bundle, bag, badge, budget, ball, blue, bootstrap, bus, busy,
-boom, bug, buffet, ban, boolean, bill, bear, blue, box, border, beautiful,
+boom, bug, buffet, ban, boolean, bill, bear, blue, box, border, beautiful, bind,
 
 cute, cow, collet, collection, coach, coast, core, cost, code, cool, cold, com, cake, car, cast, career, convert, celebrate,
 catch, camera, case, cafe, coffee, cell, carbon, calendar, cause, confirm, computer, class, classified, classification, category,
 collaboration, choose, choice, charm, corp, cover, city, configure, configuration, contribute, contribution, combine, compose, consult,
-commuicate, commuication, chair,
+commuicate, commuication, chair, clarify, clock, create, creative, creation, character,
 
 data, date, damage, definition, delegate, distation, die, dog, delete, degree, disagree, dig, device, door, dart, down, do, dial,
-door, downtown,
+door, downtown, dry, dumpling,
 
-eye, editor, edit, else, empty, effect, east, estimate, earn, earning, elephant,
+eye, editor, edit, else, empty, effect, east, estimate, earn, earning, elephant, every,
 
 far, fast, father, further, front, float, floor, flower, forgive, for, force, forever, fragment, fire, figure, french, favourite, farewell, foundation, framework,
-firework, follow, family, fade, fell, finger, fly, fee, fish,
+firework, follow, family, fade, fell, finger, fly, fee, fish, filter, function,
 
 grade, grow, garden, give, gift, gather, go, get, glad, gallery, gentle, green, grey, gray,
 
@@ -25,21 +25,21 @@ handsome, have, has, host, hostname, hile, hat,
 
 icon, information, if, illegal, illness, important, insert, increase, install,
 
-jest, jam, just, jungle,
+jest, jam, just, jungle, java,
 
 kitchen, kind, kindness, keyboard, key, knife, kindom, king,
 
 laptop, law, lawer, large, light, load, lazy, layout, layer, last, lemon, like, length, list, late, land, lord, less, least, lease,
-lion, line, light, love,
+lion, line, light, love, laugh,
 
 math, memory, method, mountain, most, more, mock, model, mode, modal, moon, moto, myth, meat, meal, mongo, middle, margin,
 
-new, never, near, nearby, nest, negative, nut,
+new, never, near, nearby, nest, negative, nut, noodle,
 
 often, omit, old, office, outline, obey,
 
 pull, push, pollute, pillar, pick, paradise, put, puppy, people, person, package, plan, pixel, promotion, ping, peace,
-play, playground, plain, physical, phone, premium, professional, pen, power, property, potato, plus, pay, paper,
+play, playground, plain, physical, phone, premium, professional, pen, power, property, potato, plus, pay, paper, prototype,
 
 queue, question, quick, quarter, quality, padding,
 
@@ -47,24 +47,23 @@ recent, recall, reply, real, really, reduce, red, renew, right, rough, rotate, r
 repository, related, road,
 
 stage, star, scroll, style, sheet, small, sea, season, select, schema, slot, sudden, spring, story, south, soft, shutdown,
-seat, slow, smooth, string, state, storm,
+seat, slow, smooth, string, state, storm, sense, stand, sum, some, sofa, string,
 
 thing, tiny, tired, traffic, tower, table, that, this, those, these, then, task, transform, translate, the, test,
 
 unique, update, urgent, unified, uninstall, united, upstair,
 
-visit, view, vehicle, visa,
+visit, view, vehicle, visa, vote, vector, volumne,
 
 water, waterfall，waiter, world, word, what, when, why, where, whether, west, warning, waste, width, week, weekend, work,
 
 xerox,
 
-year, yeal, yellow,
+year, yeal, yellow, yard,
 
-zoo, zoom
+zoo, zoom, zip, zipcode
 
 `;
 
-const knowns = new Set(words.split(','));
-
-console.log(`Total: ${knowns.size}`);
+const knowns = Array.from(new Set(words.split(',').map((x) => x.replace(/\n/g, '')))).sort();
+console.log(`Total: ${knowns.length}`);
