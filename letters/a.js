ddc0e6fe7978a806c21863b27ea6c4b79bb63312
Edmond Wang <ywang53@ebay.com>
2019-06-22 16:23:34 +0800

    separate a and b

---
+++ letters/a.js
@@ -0,0 +1,7 @@
+let letter_a = `
+    accord, apple, apply, application, arm, auto, almost, animation,
+    advertisement, alert, angry, angela, autumn, army, ask, automatic, airplane,
+    aircraft, along, align, allow, abort, accept, amazing, anticlock, array, America, above, assist
+`;
+
+module.exports = letter_a;
--- words.js
+++ words.js
@@ -1,23 +1,23 @@
-const words = `
-accord, apple, apply, application, arm, auto, almost, animation, advertisement, alert, angry, angela, autumn, army, ask, automatic, airplane,
-aircraft, along, align, allow, abort, accept, amazing, anticlock, array, America, above, assist,
+let words = '';
 
-build, base, brave, baker, big, but, because, bubble, bump, bundle, bag, badge, budget, ball, blue, bootstrap, bus, busy,
-boom, bug, buffet, ban, boolean, bill, bear, blue, box, border, beautiful, bind, boot, board, beef, bullet, below,
+['a', 'b'].forEach((letter) => {
+    words = words + (require(`./letters/${letter}`));
+});
 
+words = words + `
 cute, cow, collet, collection, coach, coast, core, cost, code, cool, cold, com, cake, car, cast, career, convert, celebrate,
 catch, camera, case, cafe, coffee, cell, carbon, calendar, cause, confirm, computer, class, classified, classification, category,
 collaboration, choose, choice, charm, corp, cover, city, configure, configuration, contribute, contribution, combine, compose, consult,
 commuicate, commuication, chair, clarify, clock, create, creative, creation, character, commit, commander, cat, curve, clone, could,
 cloud,
 
-data, date, damage, definition, delegate, distation, die, dog, delete, degree, disagree, dig, device, door, dart, down, do, dial,
+data, date, damage, definition, delegate, distation, die, dog, delete, degree, demo, disagree, dig, device, door, dart, down, do, dial,
 door, downtown, dry, dumpling, dare,
 
 eye, editor, edit, else, empty, effect, east, estimate, earn, earning, elephant, every, energy, east,
 
 far, fast, father, further, front, float, floor, flower, forgive, for, force, forever, fragment, fire, figure, french, favourite, farewell,
-foundation, framework, firework, follow, family, fade, fell, finger, fly, fee, fish, filter, function, fax, fix, fit, famous, factory, facade,
+foundation, framework, firework, follow, family, fade, fell, finger, fly, fee, fish, filter, function, fax, find, fix, fit, famous, factory, facade,
 farm, farmer,
 
 grade, grow, garden, give, gift, gather, go, get, glad, gallery, gentle, green, grey, gray, German, gold, good, gun,
@@ -31,19 +31,19 @@ jest, jam, just, jungle, java, Japan, job,
 
 kitchen, kind, kindness, keyboard, key, knife, kindom, king, Korea,
 
-laptop, law, lawer, large, light, load, lazy, lady, layout, layer, last, lemon, like, length, list, late, land, lord, less, least, lease,
+laptop, law, lawer, large, light, little, load, lazy, lady, layout, layer, last, lemon, like, length, list, late, land, lord, less, least, lease,
 lion, line, light, love, laugh, lock, left,
 
 math, memory, method, mountain, most, more, mock, model, mode, modal, moon, moto, myth, meat, meal, mongo, middle, margin, mechasim,
 mask, max, mini, mine,
 
-new, never, near, nearby, nest, negative, nut, noodle, north, noon, next,
+narrow, new, never, near, nearby, nest, negative, nut, noodle, north, noon, next,
 
 often, omit, old, office, outline, obey, operation,
 
 padding, package, pull, push, pollute, pillar, pick, paradise, put, puppy, people, person, package, plan, pixel, promotion, ping, peace,
 play, playground, plain, physical, phone, premium, professional, pen, power, property, potato, plus, pay, paper, prototype, pool, pizza,
-puppy,
+puppy, police,
 
 queue, question, quick, quarter, quality,
 
@@ -54,7 +54,7 @@ stage, star, scroll, style, sheet, small, sea, season, select, schema, slot, sud
 seat, slow, smooth, string, state, storm, sense, stand, sum, some, sofa, string, start, service, server, security, secure, survey,
 science, swim, size, status, school, sun, sunset, system,
 
-thing, tiny, tired, traffic, tower, table, that, this, those, these, then, task, transform, translate, the, test, think, thinking,
+tablet, thing, tiny, tired, traffic, tower, table, telephone, television, tell, that, this, those, these, then, task, transform, translate, the, test, think, thinking,
 triangle, tick, table,
 
 unique, update, urgent, unified, uninstall, united, upstair, United State, up, uniform, user,
